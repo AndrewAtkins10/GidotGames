8ca8fd43208be9d4bcc6a14a937a4e7482394716
Andrew Atkins <andrewsoccer10@yahoo.com>
2025-03-28 20:02:49 -0500

    pog

--- platforming/player.gd
+++ platforming/player.gd
@@ -1,31 +1,33 @@
 extends CharacterBody2D
 
+@onready var anim = $Sprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-
+const JUMP_VELOCITY = -500.0
+const GRAVITY = 980
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		anim.play("jump")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if is_on_floor():
 		if direction:
 			velocity.x = direction * SPEED
+			anim.flip_h = direction < 0  # Flip sprite for left movement
+			anim.play("walk")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED*delta)
-	else:		
-		if direction:
-			velocity.x = move_toward(velocity.x, SPEED*direction, SPEED*delta)
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED*5*delta)
+			anim.play("idle")
+	else:
+		if velocity.y > 0:
+			anim.play("jump")  # Can differentiate "jump" and "fall" if needed
 
 	move_and_slide()
